f9d4dc949b5e1987d90db70664ee82a6abbef650
edgarjt <edgarjt97@gmail.com>
2021-08-26 13:48:40 -0500

    add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  loginForm:any;
+  submitted = false;
+  disableButtonAdd: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.disableButtonAdd = false;
+    this.loginForm = FormGroup;
   }
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userEmail: new FormControl('', [Validators.required, Validators.email]),
+      userPassword: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get form() {return this.loginForm?.controls; }
+
+  login() {
+    console.log('ooko');
+  }
 
 }
